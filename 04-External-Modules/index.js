cde1bce2e0209fe89e71f6436c4510b32557cf2f
arfanovi <arfanovi.gub1@gmail.com>
2024-07-26 22:05:04 +0600

    External modules

---
+++ 04-External-Modules/index.js
@@ -0,0 +1,7 @@
+// const getRandomFruitsName = require('random-fruits-name');
+
+// console.log(getRandomFruitsName("en"));
+
+
+const getMoviesName = require('movies-names');
+console.log(getMoviesName.random())
