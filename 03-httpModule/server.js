feabd062b07ad1b1dc78c0cde76155ed88911dc0
arfanovi <arfanovi.gub1@gmail.com>
2024-07-26 21:41:27 +0600

    Status Code

--- 03-httpModule/server.js
+++ 03-httpModule/server.js
@@ -1,15 +1,31 @@
 // http modules
-
+/*
 const http = require('http');
 const port = 3000;
 const hostname = '127.0.0.1'
 
 const myServer = http.createServer((req, res) =>{
-
     res.end("Hello I'm First Server")
-
 });
 
+myServer.listen(port, hostname, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}`)
+})
+
+*/
+
+
+
+// More Http 
+const http = require('http');
+const port = 3000;
+const hostname = '127.0.0.1'
+
+const myServer = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write("<h1>Hello I'm First Server</h1>");
+    res.end()
+});
 
 myServer.listen(port, hostname, ()=>{
     console.log(`Server is running at http://${hostname}:${port}`)
