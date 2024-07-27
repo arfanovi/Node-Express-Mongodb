f0af6eb1ac0a8b772fd881e09c1f37fd1dc07c34
arfanovi <arfanovi.gub1@gmail.com>
2024-07-27 21:34:14 +0600

    Express Routing

--- 07-Create-Server/app.js
+++ 07-Create-Server/app.js
@@ -1,6 +1,11 @@
 const express =  require('express');
 const app = express();
 
+// access router folder 
+const userRouter = require('./Routes/users.route')
+
+// Know about HTTP methods 
+/*
 app.get("/", (req, res)=> {
     res.send("I am a GET request at home route")
 })
@@ -17,6 +22,14 @@ app.delete("/", (req, res)=> {
     res.send("I am a Delete request at Home route")
 })
 
+*/
 
+
+// Use this access routes 
+app.use("/api/user", userRouter);
+
+app.use((req,res)=>{
+    res.send("<h1>404 Not valid URL</h1>")
+})
 
 module.exports = app;
