dd11ebba68979d823ea002e303d81a4835b76663
arfanovi <arfanovi.gub1@gmail.com>
2024-07-28 15:00:40 +0600

    Route prameter and Header

--- 09-HTTP-Request/index.js
+++ 09-HTTP-Request/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-
+/*
 app.get("/", (req,res)=>{
     // const id = req.query.id;
     // const name = req.query.name;
@@ -11,6 +11,30 @@ app.get("/", (req,res)=>{
     const {id, name} = req.query;
     res.send(`Student name is ${name} and id is ${id}`)
     // res.send("I'm Home route")
+})
+
+*/
+
+
+
+// Route Param 
+/*
+app.get("/userId/:id/userAge/:age", (req,res)=>{
+
+    const id = req.params.id;
+    const age = req.params.id;
+    res.send(`Student id is ${id} and age is ${age}`)
+
+})
+*/
+
+
+// Headers 
+
+app.get("/", (req,res)=>{
+    const id = req.header('id');
+    const name = req.header('name')
+    res.send(`Student id is ${id} and name is ${name}`)
 })
 
 app.listen(PORT, ()=>{
